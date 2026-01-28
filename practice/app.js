acda36be8943bb0cdce3ffde5d1d58685824d238
Dev-UV3 <coderuv3@gmail.com>
2026-01-28 23:59:16 +0530

    practicing js

---
+++ practice/app.js
@@ -0,0 +1,21 @@
+document.body.style.backgroundColor = "black";
+
+
+// const div = document.createElement("div");
+
+//     div.textContent = "Hello World!";
+//     div.style.color = "blue";
+//     div.style.fontSize="50px";
+
+//     document.body.appendChild(div);
+
+
+// const container = document.querySelector("#container");
+
+// const content = document.createElement("div");
+// content.classList.add("content");
+// content.style.color="white";
+// content.textContent = "This is the glorious text-content!";
+// content.style.fontSize="40px";
+
+// container.appendChild(content);
